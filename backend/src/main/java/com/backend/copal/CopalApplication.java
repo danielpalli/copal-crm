3c89cc02b1828a842a4038a688871686cc73b37b
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2024-02-01 21:12:25 -0300

    Agregado proyectos iniciales backend y frontend

---
+++ backend/src/main/java/com/backend/copal/CopalApplication.java
@@ -0,0 +1,13 @@
+package com.backend.copal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopalApplication.class, args);
+	}
+
+}
