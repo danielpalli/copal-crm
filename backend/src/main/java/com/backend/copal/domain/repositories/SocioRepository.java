c67c4a6814c0250bdab613b3ffdf8399ae4a1c8e
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2024-02-28 18:16:48 -0300

    Update

--- backend/src/main/java/com/backend/copal/domain/repositories/SocioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backend.copal.domain.repositories;
-
-import com.backend.copal.domain.entities.Socio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SocioRepository extends JpaRepository<Socio, Long> {}
--- backend/src/main/java/com/backend/copal/infrastructure/abstract_services/ISocioService.java
+++
@@ -1,7 +0,0 @@
-package com.backend.copal.infrastructure.abstract_services;
-
-import com.backend.copal.domain.dtos.socio.SocioRequest;
-import com.backend.copal.domain.dtos.socio.SocioResponse;
-
-public interface ISocioService extends CrudService<SocioRequest, SocioResponse, Long> {
-}
